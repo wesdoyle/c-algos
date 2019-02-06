7448bfdcf2b7caff9552f688102dfb68c825fde8
Wes Doyle <wesdoyle08@gmail.com>
2019-02-05 18:03:39 -0600

    Add add_node

--- linked-list-city-population.c
+++ linked-list-city-population.c
@@ -1,6 +1,7 @@
 #include "linked-list-city-population.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*--- linked-list-city-population.c ---
  * Reads a text file of city names and populations in the format:
@@ -26,7 +27,7 @@ int node_count;
 int add_node (link);
 void create_list (void);
 int delete_node (void);
-int duplicate_node (void);
+int duplicate_node (link, link);
 void free_node (void);
 void show_nodes (void);
 int compare_nodes (link, link);
@@ -36,4 +37,37 @@ int add_node(link to_add)
     link copy, prev, curr;
     struct node dummy;
     int i;
+
+    // copy the input node
+    copy = (link) malloc(sizeof(struct node));
+    if (copy == NULL)
+        return 0;
+
+    memcpy(copy, to_add, sizeof(struct node));
+
+    // create a dummy node
+    dummy.next = head;
+    prev = &dummy;
+    curr = head;
+
+    // insert node copy
+    for (;; prev=curr, curr=curr->next)
+    {
+        if (curr == NULL)
+            break; // the end has been reached
+        i = compare_nodes(copy, curr);
+
+        if (i <= 0)
+            break; // copy precedes current
+    }
+
+    if (curr && i == 0) // duplicate
+        if (duplicate_node(curr, copy) == 0)
+            return (1);
+
+    prev -> next = copy;
+    copy -> next = curr;
+    head = dummy.next;
+
+    return (1);
 }
